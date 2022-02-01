6ae03eef5a93943c39d93cdde0a8e4c4b4062b2c
billsimon <billsimon@users.noreply.github.com>
2022-02-01 12:28:08 -0500

    Create transcribe.php

---
+++ transcribe.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+
+$msSpeechUrl = 'https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US';
+$apiKey = 'YOUR-KEY-HERE';
+
+$maildata = file_get_contents("php://stdin");
+$msg = preg_split("/\r\n|\n|\r/", $maildata);
+$att = '';
+for($i = 0; $i < count($msg); $i++) {
+        if (preg_match('/Content-Disposition: attachment/', $msg[$i])) {
+                $i++; // skip to next line
+                while (! preg_match('/----/', $msg[$i])) {
+                        $att .= $msg[$i] . "\n";
+                        $i++;
+                }
+        }
+}
+
+if ($att) {
+        $opts = array('http' =>
+                array(
+                        'method' => 'POST',
+                        'header' => array(
+                                "Ocp-Apim-Subscription-Key: $apiKey",
+                                'Content-Type: audio/wav'
+                                ),
+                        'content' => base64_decode($att)
+                )
+        );
+        $context = stream_context_create($opts);
+        $result = json_decode(file_get_contents($msSpeechUrl, false, $context));
+}
+
+if (! empty($result->{"DisplayText"})) {
+        $maildata = str_replace('(TRANSCRIPTION)', $result->{"DisplayText"}, $maildata);
+} else {
+        $maildata = str_replace('(TRANSCRIPTION)', 'No transcription availabile.', $maildata);
+}
+
+$mailproc = popen('/usr/sbin/sendmail -t', 'w');
+fwrite($mailproc, $maildata);
+pclose($mailproc);
+
+exit;
+?>
